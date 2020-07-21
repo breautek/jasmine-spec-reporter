a1352c0d3bed195b2d8e65ccd2dc45da582f146b
Norman Breau <norman@normanbreau.com>
2020-07-21 17:05:20 -0300

    progress

--- src/display-processor.ts
+++ src/display-processor.ts
@@ -1,15 +1,29 @@
 import {Configuration} from "./configuration";
 import {CustomReporterResult} from "./spec-reporter";
+import {Bar, Presets} from "cli-progress";
 import SuiteInfo = jasmine.SuiteInfo;
 
 export class DisplayProcessor {
     protected configuration: Configuration;
 
+    private _progress: Bar;
+    private _totalSpecs: number;
+    private _progressValue: number;
+
     constructor(configuration: Configuration) {
         this.configuration = configuration;
+        this._progressValue = 0;
+        this._progress = new Bar({
+            format: "[{bar}] {value}/{total}    {text}"
+        },                       Presets.shades_classic);
     }
 
     public displayJasmineStarted(info: SuiteInfo, log: string): string {
+        this._totalSpecs = info.totalSpecsDefined;
+        this._progress.setTotal(this._totalSpecs);
+        this._progress.update(this._progressValue, {
+            text: "Jasmine Started"
+        });
         return log;
     }
 
@@ -18,6 +32,9 @@ export class DisplayProcessor {
     }
 
     public displaySpecStarted(spec: CustomReporterResult, log: string): string {
+        this._progress.update(++this._progressValue, {
+            text: spec.fullName
+        });
         return log;
     }
 
